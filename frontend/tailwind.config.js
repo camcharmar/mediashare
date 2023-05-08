f372c1cee3f54cc0aab8b4aa9e95074a4fb4d197
Jacob Dale <jacobscottdale@gmail.com>
2023-05-08 13:41:41 -0400

    Refactored to TypeScript boilerplate

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,13 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    content: ['./src/**/*.js'],
+    content: [
+        // "./app/**/*.{js,ts,jsx,tsx}",
+        // "./pages/**/*.{js,ts,jsx,tsx}",
+        // "./components/**/*.{js,ts,jsx,tsx}",
+
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
     darkMode: 'media',
     theme: {
         extend: {
